d25a01a112ce1f75cfdee3c00a7c3f8dca464fcd
thiagoBiancareli <thiagob2mt@gmail.com>
2022-11-09 11:23:43 -0300

    Imagens, links e carrinho feitoi

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 // - Importar o express
 const express = require("express");
-
+const path = require("path");
 // - Criar o servidor app
 const servidor = express();
 
-// - Definir rota neste servidor
+// - Definir a pasta public como sendo a pasta de arquivos estáticos
+servidor.use(express.static(path.join(__dirname, 'public')))
+// servidor.use(express.static(path.join(__dirname, 'carrinho')))
+
 // - Endereço, método, função callback (acão que o servidor vai realizar qiando a req chegar);
-servidor.get('/usuarios', (req, res)=>{
+servidor.get('/', (req, res)=>{
     console.log("Chegou uma requisição");
     //return res.send("Permaneça em linha!");
+
     res.sendFile(__dirname + '/views/index.html')
 });
+
+servidor.get('/carrinho',(req, res)=>{
+    return res.sendFile(__dirname + '/views/carrinho.html');
+})
 
 // - Por o servidor pra rodar "Aguardando requisição"
 servidor.listen(3000);
