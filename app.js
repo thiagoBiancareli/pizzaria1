27c7b05fb99cf57e123663d005ff729cb31fb82a
thiagoBiancareli <thiagob2mt@gmail.com>
2022-12-03 15:16:00 -0300

    Media Queries, Layouts responsivos, Flexbox, formulários

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const servidor = express();
 
 // - Definir a pasta public como sendo a pasta de arquivos estáticos
 servidor.use(express.static(path.join(__dirname, 'public')))
-// servidor.use(express.static(path.join(__dirname, 'carrinho')))
 
 // - Endereço, método, função callback (acão que o servidor vai realizar qiando a req chegar);
 servidor.get('/', (req, res)=>{
@@ -18,6 +17,10 @@ servidor.get('/', (req, res)=>{
 
 servidor.get('/carrinho',(req, res)=>{
     return res.sendFile(__dirname + '/views/carrinho.html');
+})
+
+servidor.get('/perfil',(req, res)=>{
+    return res.sendFile(__dirname + '/views/perfil.html')
 })
 
 // - Por o servidor pra rodar "Aguardando requisição"
